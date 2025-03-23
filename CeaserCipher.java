dce4c04b89b6dafd95c674407a017a3cf32adc34
GhanIs05 <154982500+GhanIs05@users.noreply.github.com>
2025-03-23 17:50:35 +0530

    Update CeaserCipher.java

--- CeaserCipher.java
+++ CeaserCipher.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class CeaserCipher
 {
-    // Encrypts text using a shift of s
     public static StringBuffer encrypt(String text, int s)
     {
         StringBuffer result = new StringBuffer();
@@ -30,13 +29,11 @@ public class CeaserCipher
         {
             if (Character.isUpperCase(text.charAt(i)))
             {
-                // Decrypt upper-case letters by shifting backwards
                 char ch = (char)(((int)text.charAt(i) - s - 65 + 26) % 26 + 65);
                 result.append(ch);
             }
             else
             {
-                // Decrypt lower-case letters by shifting backwards
                 char ch = (char)(((int)text.charAt(i) - s - 97 + 26) % 26 + 97);
                 result.append(ch);
             }
@@ -44,23 +41,18 @@ public class CeaserCipher
         return result;
     }
 
-    // Driver code
     public static void main(String[] args)
     {
-        // Create scanner object for input
         Scanner sc = new Scanner(System.in);
 
-        // Accepting user input for text and shift
         System.out.print("Enter the text to encrypt: ");
         String text = sc.nextLine();
 
         System.out.print("Enter the shift value: ");
         int s = sc.nextInt();
 
-        // Close the scanner
         sc.close();
 
-        // Print the results
         System.out.println("Text  : " + text);
         System.out.println("Shift : " + s);
         System.out.println("Cipher: " + encrypt(text, s));
